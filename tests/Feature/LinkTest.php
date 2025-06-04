30361a01aa5cfd90278e657cbeea57ee6ede36ad
Taylor <taylormdaniels1@gmail.com>
2025-06-04 16:48:00 -0400

    Quick test

---
+++ tests/Feature/LinkTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Link;
+
+class LinkTest extends TestCase
+{
+  public function it_shortens_and_redirects(){
+    $response = $this->postJson('/api/links', ['url' => 'https://laravel.com']);
+    $response->assertCreated()->json('short');
+
+    $code = Link::first()->code;
+    $this->get("/{$code}")->assertRedirect('https://laravel.com');
+
+    $this->assertDatabaseCount('clicks', 1);
+  }
+}
